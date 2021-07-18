19d3de4d4df2c7fc2be3b22ab3763d89ba14d9ac
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-07-18 11:21:53 -0400

    upld logo moved folder into src

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,12 @@
+import logo from './images/Syro-waste Directory'
+
 function Header() {
   return (
     <header className="sticky-top">
     {/* NavBar Beginning*/}
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-light py-3">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">B3</a>
+            <a className="navbar-brand mx-auto" href="">B3</a> {/*are the a tags supposed to be link tags in react?*/}
             {/* Replace brand w logo, change nav clr */}
         </div>
     </nav>
