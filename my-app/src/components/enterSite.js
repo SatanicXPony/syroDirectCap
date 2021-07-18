b0417e568a1bcfaa1b4ef1e9c22559cf784647dc
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-07-18 14:26:44 -0400

    set up for main page transition w frmr

---
+++ my-app/src/components/enterSite.js
@@ -0,0 +1,25 @@
+{/* src: page transitions in react - DEV.to
+
+* import { motion, AnimatePresence } from 'framer-motion'
+* import {capLogo} from '../assets/images/capLogo.png' < this needs to be the animated version of logo on canva
+
+* i want to use the logo i made on canva and set it as the transition
+
+*const blueBox = {
+  initial: {
+    height: "100vh",
+    right: 0,
+  },
+  animate: {
+    height: 0,
+    transition: {
+      duration: 2.5
+    }
+  }
+}
+* const siteTransition = () => {
+
+}
+
+
+*/}
