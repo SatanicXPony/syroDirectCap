d2c04756ecc63acd4fd3c47ba0dfd96dab853500
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-07-18 12:29:26 -0400

    logo is here, but v small

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,13 +1,15 @@
-import slide1  from '../public/slide1.jpg'
+import capLogo from '../assets/images/capLogo.png';
+
+{/*will i need to useState for every page that uses reactstrap?*/}
 
 function Header() {
   return (
     <header className="sticky-top">
     {/* NavBar Beginning*/}
-    <nav className="navbar navbar-expand-lg navbar-dark bg-light py-3">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
         <div className="container-fluid">
-            <a className="navbar-brand mx-auto" href="#">
-              <img src="/public/slide1.jpg" width="50" height="40"/>
+            <a className="navbar-brand mx-auto" href="/">
+              <img src={capLogo} alt="mainLogo" width="90px" height="90px"/>
               </a> {/*are the a tags supposed to be link tags in react?*/}
             {/* Replace brand w logo, change nav clr */}
         </div>
