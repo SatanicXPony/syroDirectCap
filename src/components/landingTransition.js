50849413c38bb4409fb04dbd04574973d785d87b
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-08-16 14:56:30 -0400

    attempt to save changes

---
+++ src/components/landingTransition.js
@@ -0,0 +1,39 @@
+{/* src: page transitions in react - DEV.to
+* i want to use the logo i made on canva and set it as the transition
+* need the animated version of logo on canva
+all of the pages, their routes will need to be wrapped in animate presence*/}
+
+
+{/* import { motion } from 'framer-motion'
+//import { capLogo } from '../assets/images/capLogo.png' 
+
+
+const blueBox = {
+  initial: {
+    height: "100vh",
+    right: 0,
+  },
+  animate: {
+    height: 0,
+    transition: {
+      ease: "easeOut", 
+      duration: 3
+    },
+  },
+},
+
+const landingTransition = () => {
+  return (
+    <div className="absolute inset-0 flex">
+      <motion.div 
+        className="relative z-40 w-full bg-blue"
+        initial="initial"
+        animate="animate"
+          variants={blueBox}
+      />
+    </div>
+  );
+};
+
+export default landingTransition; */}
+
