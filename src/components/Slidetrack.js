861710bb6dc017aba96a1babee89799c46803306
mackTheKid <45269241+SatanicXPony@users.noreply.github.com>
2021-08-17 14:20:00 -0400

    more changes to dummy data

--- src/components/Slidetrack.js
+++ src/components/Slidetrack.js
@@ -7,10 +7,15 @@ SwiperCore.use([Navigation, Pagination]);
 function Cardbuilder() {
   const [cards, setCards] = useState([
     { title: "Follow The Orange", 
-    img: "https://images.app.goo.gl/dT3VhMYMmM2TYRia8", 
-    url: "https://wastefreephd.com/2020/03/01/follow-the-orange-a-guide-to-waste-reduction-in-syracuse-ny/", 
-    description: "A local guide to sustainability resources" },
-    { title: "e", img: "https://via.placeholder.com/100", url: "g", description: "h" },
+      img: "https://images.app.goo.gl/dT3VhMYMmM2TYRia8", 
+      url: "https://wastefreephd.com/2020/03/01/follow-the-orange-a-guide-to-waste-reduction-in-syracuse-ny/", 
+      description: "A local guide to sustainability resources" 
+    },
+    { title: "Apple Store Destiny USA", 
+      img: "https://images.app.goo.gl/k6UnpJpvWuoyZwS26", 
+      url: "https://www.apple.com/recycling/nationalservices/", 
+      description: "Apple Store Recycling Program" 
+    },
     { title: "i", img: "https://via.placeholder.com/100", url: "k", description: "l" },
     { title: "i", img: "https://via.placeholder.com/100", url: "k", description: "l" },
     { title: "i", img: "https://via.placeholder.com/100", url: "k", description: "l" },
